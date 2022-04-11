2749639d8bf1b9096bb8aad84b41e6eab6f7adbb
Brandon Lanier <brandonjlanier@gmail.com>
2022-04-11 14:33:54 -0500

    messing with the sql file

--- server/routes/questions.router.js
+++ server/routes/questions.router.js
@@ -2,8 +2,9 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-
-
+router.get('/', (req, res) => {
+    const sqlTxt = `SELECT * FROM `
+})
 
 
 module.exports = router;
