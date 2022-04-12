1de66dc4129a7e9799ee98f4ccd66966c6e0b81b
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-04-12 14:49:02 -0500

    created scores axios.post

--- server/routes/questions.router.js
+++ server/routes/questions.router.js
@@ -17,24 +17,9 @@ router.get('/', (req, res) => {
 
 
 // this post is under development // Awaiting info on how the client side will send data.
-let currentDate = new Date();
 
-router.post('/', (req, res) => {
-    const answers = req.body;
-    const user = req.user.id;
-    const date = currentDate;
-    const qryTxt = `
-    INSERT INTO "scores" ("userId", "assessmentBatchId", "questionId", "score", "scoreQualitative", "date")
-    VALUES ($1, $2, $3, $4, $5, $6, $7)
-    ;`
-    pool.query(qryTxt, [user, answers.batch, answers.id, answers.score, answers.qualitative, date ])
-    .then(result => {
-        res.sendStatus(201)
-    }).catch(err => {
-        console.log('Error in question post route', err);
-        res.sendStatus(500);
-    })
-})
+
+
 
 // CREATE TABLE "scores" (
 // 	"id" SERIAL NOT NULL,
