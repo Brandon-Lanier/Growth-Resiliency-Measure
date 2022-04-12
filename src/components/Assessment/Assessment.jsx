3dbc9f481657817840295a89ada641140277f687
Brandon Lanier <brandonjlanier@gmail.com>
2022-04-12 10:51:20 -0500

    Building out assessment

---
+++ src/components/Assessment/Assessment.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+
+function Assessment() {
+    
+
+
+    useEffect(() => {
+        dispatch
+    })
+
+
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+
+export default Assessment;
