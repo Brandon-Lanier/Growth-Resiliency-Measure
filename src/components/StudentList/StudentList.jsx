1e09d21e62ed4b0762efb7a5d48d971d6fccae0d
Adam Garberg <adamgarberg@gmail.com>
2022-04-12 16:10:54 -0500

    studentlist showing from DB

--- src/components/StudentList/StudentList.jsx
+++ src/components/StudentList/StudentList.jsx
@@ -6,7 +6,8 @@ import CsvUpload from '../CsvUpload/CsvUpload';
 function StudentList() {
 
   const store = useSelector((store) => store);
-
+  const students = useSelector((store) => store.studentReducer);
+  console.log(students)
   const columns = [
     {
         field: 'fullName',
@@ -18,38 +19,56 @@ function StudentList() {
           `${params.row.firstName || ''} ${params.row.lastName || ''}`,
       },
     {
-      field: 'gradDate',
-      headerName: 'Grad Date',
+      field: 'graduationYear',
+      headerName: 'Grad Year',
       width: 110,
       editable: true,
     },
     {
-        field: 'school',
+        field: 'schoolId',
         headerName: 'School',
         width: 110,
         editable: true,
       },
-
+      {
+        field: 'email',
+        headerName: 'Email',
+        width: 110,
+        editable: true,
+      },
+      {
+        field: 'race',
+        headerName: 'Race',
+        width: 110,
+        editable: true,
+      },
+      {
+        field: 'gender',
+        headerName: 'Gender',
+        width: 110,
+        editable: true,
+      },
+      {
+        field: 'lunchStatus',
+        headerName: ' Lunch Status',
+        width: 110,
+        editable: true,
+      },
+      {
+        field: 'eip',
+        headerName: ' EIP',
+        width: 110,
+        editable: true,
+      },
   ];
 
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', school: 'SaintPaul', gradDate: 2023 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei',school: 'Chanhassen', gradDate: 2028 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime',school: 'Edina', gradDate: 2024 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya',school: 'Chaska', gradDate: 2026 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys',school: 'Minnetonka', gradDate: 2026 },
-    { id: 6, lastName: 'Melisandre', firstName: 'Meliassa',school: 'Hopkins', gradDate: 2025 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara',school: 'Minneapolis', gradDate: 2022 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini',school: 'South', gradDate: 2027 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey',school: 'Eden Prairie', gradDate: 2028 },
-  ];
 
   return (
       <>
      <CsvUpload/>
     <div style={{ height: 700, width: '100%' }}>
       <DataGrid
-        rows={rows}
+        rows={students}
         columns={columns}
         pageSize={20}
         rowsPerPageOptions={[20]}
