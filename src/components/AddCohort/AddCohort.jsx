60a6a99d12b3ab437eae268e7cde999187cdf960
Ben Pedrick <bpedrick@gmail.com>
2022-04-12 14:29:19 -0500

    front end new cohort form built

--- src/components/AddCohort/AddCohort.jsx
+++ src/components/AddCohort/AddCohort.jsx
@@ -1,64 +1,91 @@
-// 
-// This component creates a new cohort for tests to the database. It should end up creating two lines in the 
+//
+// This component creates a new cohort for tests to the database. It should end up creating two lines in the
 // AssessmentBatches table
 //
 //
 //
 
-import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
-import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import axios from "axios";
 
 function AddCohort(props) {
+  //   const store = useSelector((store) => store);
+  const [heading, setHeading] = useState("Add Assessment Cohort");
 
-//   const store = useSelector((store) => store);
-  const [heading, setHeading] = useState('Add Assessment Cohort');
+  const [value1, setValue1] = useState(new Date());
+  const [value2, setValue2] = useState(new Date());
+  const [valueYear, setValueYear] = useState(new Date());
 
-  const [value, setValue] = useState(new Date('2014-08-18T21:11:54'));
+  const handleSubmit = () => {
+    const newBatch = {
+      startDate: value1.toISOString().split("T")[0],
+      endDate: value2.toISOString().split("T")[0],
+      fiscalYear: valueYear.toISOString().substring(0, 4),
+    };
+    console.log("New batch is", newBatch);
+    try {
+        axios.post("/api/admin/cohort", newBatch);
 
-  const handleChange = (newValue) => {
-    setValue(newValue);
+    } catch (error) {
+        console.log('error on batch post',error);
+    }
+
+
   };
-
 
   return (
     <div>
-        <Box sx={{
-          display: 'flex',
-          flexDirection: 'column',
-
-          justifyContent: 'center'
-      }}>
-
       <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <h2>{heading}</h2>
-      <DatePicker
-        label="Semester Start"
-        value={value}
-        onChange={(newValue) => {
-            setValue(newValue);
-        }}
-        renderInput={(params) => <TextField {...params} />}
-        />
+        <Box
+          sx={{
+            boxShadow: 1,
+            p: 5,
+          }}
+        >
+          <Stack spacing={3}>
+            <h2>{heading}</h2>
             <DatePicker
-        label="Semester End"
-        value={value}
-        onChange={(newValue) => {
-            setValue(newValue);
-        }}
-        renderInput={(params) => <TextField {...params} />}
-        />
-    </LocalizationProvider>
+              label="Semester Start"
+              value={value1}
+              margin={2}
+              onChange={(newValue) => {
+                setValue1(newValue);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+            <DatePicker
+              label="Semester End"
+              value={value2}
+              onChange={(newValue) => {
+                setValue2(newValue);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+            <DatePicker
+              views={["year"]}
+              label="School Year"
+              value={valueYear}
+              onChange={(newValue) => {
+                setValueYear(newValue);
+              }}
+              renderInput={(params) => (
+                <TextField {...params} helperText={null} />
+              )}
+            />
+            <Button variant="contained" onClick={handleSubmit}>
+              Submit
+            </Button>
+          </Stack>
         </Box>
+      </LocalizationProvider>
     </div>
   );
 }
