321fc88d402a3e0959e9db1564a11ece06f90fb1
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-04-15 16:24:00 -0500

    csv export data dump works

--- server/routes/studentCsv.router.js
+++ server/routes/studentCsv.router.js
@@ -9,129 +9,6 @@ const encryptLib = require("../modules/encryption");
 const { batch } = require("react-redux");
 
 
-//-------------------------------Get route for scores CSV export-----------------------------------
-// sends scores table
-router.get('/scores', (req, res) => {
-    console.log('in router.get')
-    const qryTxt = `
-      SELECT * From "scores";`;
-    pool.query(qryTxt)
-      .then((result) => {
-        res.send(result.rows); 
-        console.log('result', result.rows);
-
-      })
-      .catch((err) => {
-        res.sendStatus(500);
-      });
-
-}); // end Get student Csv export---
-
-
-//-------------------------------Get route for student average scores NESTED OBJECTS-----------------------------------
-// sends average scores by measure for each student and assessment
-router.get('/nestedObjects', async (req, res) => {
-
-  // pulling students ids from db
-  const studentQryTxt = `
-  SELECT "students"."studentId"
-  FROM "students";`;
-  const studentsData = await pool.query(studentQryTxt);
-  const students = studentsData.rows
-
-  // pulling batch ids from db
-  const batchQryTxt = `
-  SELECT "batchNumber" FROM "assessmentBatches";`;
-  const batchesData = await pool.query(batchQryTxt);
-  const batches = batchesData.rows
-
-  // pulling students scores from db
-  const scoreQryTxt = `
-  SELECT "scores"."userId", "scores"."score", "scores"."scoreQualitative", "scores"."assessmentBatchId", "questions"."measureName", "students"."firstName", "students"."lastName", "students"."graduationYear", "students"."studentId", "questions"."id" AS "questionId"
-  FROM "scores"
-  JOIN "students" ON "students"."userId" = "scores"."userId"
-  JOIN "questions" ON "questions"."id" = "scores"."questionId";`;
-  const scoresData = await pool.query(scoreQryTxt);
-  const scores = scoresData.rows
-
-  // creating objects for each student for each batch which contain answers for each question
-  let allScores = []
-  for (let student of students) {
-    let studentObj = {
-      studentId: student.studentId,
-      batches: []
-    }
-    for (let batch of batches) {
-      let batchObj = {
-        batchId: batch.batchNumber,
-        scores: {
-          1: '',
-          2: '',
-          3: '',
-          4: '',
-          5: '',
-          6: '',
-          7: '',
-          8: '',
-          9: '',
-          10: '',
-          11: '',
-          12: '',
-          13: '',
-          14: '',
-          15: '',
-          16: '',
-          17: '',
-          18: '',
-          19: '',
-          20: '',
-          21: '',
-          22: '',
-          23: '',
-          24: '',
-          25: '',
-          26: '',
-          27: ''
-        }
-      }
-      for (score of scores) {
-        if (score.studentId === student.studentId && score.assessmentBatchId === batch.batchNumber) {
-          if (score.questionId != 27) {
-            batchObj.scores[score.questionId] = score.score
-          } else {
-            batchObj.scores[27] = score.scoreQualitative
-          }
-        }
-      }
-      let batchAverage = {
-        batchId: batchObj.batchId,
-        scores: {
-          balanced: (batchObj.scores[1] + batchObj.scores[2] + batchObj.scores[3]) / 3,
-          selfConfidence: (batchObj.scores[4] + batchObj.scores[5]) / 2,
-          understandingAdaptability: (batchObj.scores[6] + batchObj.scores[7] + batchObj.scores[8]) / 3,
-          connection: (batchObj.scores[9] + batchObj.scores[10] + batchObj.scores[11] + batchObj.scores[12]+ batchObj.scores[13]) / 5,
-          contribution: (batchObj.scores[14] + batchObj.scores[15]) / 2,
-          empathy: (batchObj.scores[16] + batchObj.scores[17] + batchObj.scores[18] + batchObj.scores[19]) / 4,
-          selfExpression: (batchObj.scores[20] + batchObj.scores[21] + batchObj.scores[22]) / 3,
-          selfControl: (batchObj.scores[23] + batchObj.scores[24] + batchObj.scores[25] + batchObj.scores[26]) / 4,
-          qualitative: batchObj.scores[27]
-        }
-      }
-      studentObj.batches.push(batchAverage);
-    }
-    allScores.push(studentObj)
-  }
-
-
-  // sends aggregate scores for each measure, for each student, for each batch
-  try {
-    res.send(allScores);
-  } catch (err) {
-    res.sendStatus(500);
-  }
-
-}); // end Get student Csv export NESTED OBJECTS---
-
 //-------------------------------Get route for student CSV export averaged scores-----------------------------------
 // sends average scores by measure for each student and assessment
 router.get('/', async (req, res) => {
@@ -287,5 +164,130 @@ router.post("/", async (req, res) => {
     res.sendStatus(200);
   }
 }); // End Post route
+
+
+// // NOT CURRENTLY USING ROUTES BELOW
+// //-------------------------------Get route for scores CSV export-----------------------------------
+// // sends scores table
+// router.get('/scores', (req, res) => {
+//   console.log('in router.get')
+//   const qryTxt = `
+//     SELECT * From "scores";`;
+//   pool.query(qryTxt)
+//     .then((result) => {
+//       res.send(result.rows); 
+//       console.log('result', result.rows);
+
+//     })
+//     .catch((err) => {
+//       res.sendStatus(500);
+//     });
+
+// }); // end Get student Csv export---
+
+
+// //-------------------------------Get route for student average scores NESTED OBJECTS-----------------------------------
+// // sends average scores by measure for each student and assessment
+// router.get('/nestedObjects', async (req, res) => {
+
+// // pulling students ids from db
+// const studentQryTxt = `
+// SELECT "students"."studentId"
+// FROM "students";`;
+// const studentsData = await pool.query(studentQryTxt);
+// const students = studentsData.rows
+
+// // pulling batch ids from db
+// const batchQryTxt = `
+// SELECT "batchNumber" FROM "assessmentBatches";`;
+// const batchesData = await pool.query(batchQryTxt);
+// const batches = batchesData.rows
+
+// // pulling students scores from db
+// const scoreQryTxt = `
+// SELECT "scores"."userId", "scores"."score", "scores"."scoreQualitative", "scores"."assessmentBatchId", "questions"."measureName", "students"."firstName", "students"."lastName", "students"."graduationYear", "students"."studentId", "questions"."id" AS "questionId"
+// FROM "scores"
+// JOIN "students" ON "students"."userId" = "scores"."userId"
+// JOIN "questions" ON "questions"."id" = "scores"."questionId";`;
+// const scoresData = await pool.query(scoreQryTxt);
+// const scores = scoresData.rows
+
+// // creating objects for each student for each batch which contain answers for each question
+// let allScores = []
+// for (let student of students) {
+//   let studentObj = {
+//     studentId: student.studentId,
+//     batches: []
+//   }
+//   for (let batch of batches) {
+//     let batchObj = {
+//       batchId: batch.batchNumber,
+//       scores: {
+//         1: '',
+//         2: '',
+//         3: '',
+//         4: '',
+//         5: '',
+//         6: '',
+//         7: '',
+//         8: '',
+//         9: '',
+//         10: '',
+//         11: '',
+//         12: '',
+//         13: '',
+//         14: '',
+//         15: '',
+//         16: '',
+//         17: '',
+//         18: '',
+//         19: '',
+//         20: '',
+//         21: '',
+//         22: '',
+//         23: '',
+//         24: '',
+//         25: '',
+//         26: '',
+//         27: ''
+//       }
+//     }
+//     for (score of scores) {
+//       if (score.studentId === student.studentId && score.assessmentBatchId === batch.batchNumber) {
+//         if (score.questionId != 27) {
+//           batchObj.scores[score.questionId] = score.score
+//         } else {
+//           batchObj.scores[27] = score.scoreQualitative
+//         }
+//       }
+//     }
+//     let batchAverage = {
+//       batchId: batchObj.batchId,
+//       scores: {
+//         balanced: (batchObj.scores[1] + batchObj.scores[2] + batchObj.scores[3]) / 3,
+//         selfConfidence: (batchObj.scores[4] + batchObj.scores[5]) / 2,
+//         understandingAdaptability: (batchObj.scores[6] + batchObj.scores[7] + batchObj.scores[8]) / 3,
+//         connection: (batchObj.scores[9] + batchObj.scores[10] + batchObj.scores[11] + batchObj.scores[12]+ batchObj.scores[13]) / 5,
+//         contribution: (batchObj.scores[14] + batchObj.scores[15]) / 2,
+//         empathy: (batchObj.scores[16] + batchObj.scores[17] + batchObj.scores[18] + batchObj.scores[19]) / 4,
+//         selfExpression: (batchObj.scores[20] + batchObj.scores[21] + batchObj.scores[22]) / 3,
+//         selfControl: (batchObj.scores[23] + batchObj.scores[24] + batchObj.scores[25] + batchObj.scores[26]) / 4,
+//         qualitative: batchObj.scores[27]
+//       }
+//     }
+//     studentObj.batches.push(batchAverage);
+//   }
+//   allScores.push(studentObj)
+// }
+
+
+// // sends aggregate scores for each measure, for each student, for each batch
+// try {
+//   res.send(allScores);
+// } catch (err) {
+//   res.sendStatus(500);
+// }
+
+// }); // end Get student Csv export NESTED OBJECTS---
 
 module.exports = router;
