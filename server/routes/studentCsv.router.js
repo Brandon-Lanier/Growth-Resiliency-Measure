612b2c41451e4ba0708b039bbb1756ec06961ddc
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-04-15 11:25:46 -0500

    created array of students with scores tied to student ids

--- server/routes/studentCsv.router.js
+++ server/routes/studentCsv.router.js
@@ -9,57 +9,137 @@ const encryptLib = require("../modules/encryption");
 
 
 //-------------------------------Get route for student CSV export-----------------------------------
-router.get('/', (req, res) => {
-    console.log('in router.get')
-    const qryTxt = `
-      SELECT * From "scores";`;
-    pool.query(qryTxt)
-      .then((result) => {
-        res.send(result.rows); 
-        console.log('result', result.rows);
-        
-      })
-      .catch((err) => {
-        res.sendStatus(500);
-      });
- 
-}); // end Get student Csv export---
+// router.get('/', (req, res) => {
+//     console.log('in router.get')
+//     const qryTxt = `
+//       SELECT * From "scores";`;
+//     pool.query(qryTxt)
+//       .then((result) => {
+//         res.send(result.rows); 
+//         console.log('result', result.rows);
 
+//       })
+//       .catch((err) => {
+//         res.sendStatus(500);
+//       });
+
+// }); // end Get student Csv export---
+
+
+//-------------------------------Get route for student CSV export-----------------------------------
+router.get('/', async (req, res) => {
+
+  // pulling students ids from db
+  const studentQryTxt = `
+  SELECT "students"."studentId"
+  FROM "students";`;
+  const studentsData = await pool.query(studentQryTxt);
+  const students = studentsData.rows
+
+  // pulling students scores from db
+  const scoreQryTxt = `
+  SELECT "scores"."userId", "scores"."score", "scores"."assessmentBatchId", "questions"."measureName", "students"."firstName", "students"."lastName", "students"."graduationYear", "students"."studentId", "questions"."id" AS "questionId"
+  FROM "scores"
+  JOIN "students" ON "students"."userId" = "scores"."userId"
+  JOIN "questions" ON "questions"."id" = "scores"."questionId";`;
+  const scoresData = await pool.query(scoreQryTxt);
+  const scores = scoresData.rows
+
+  let allScores = []
+
+  // averaging student scores
+  for (let student of students) {
+    let aggScore = {
+      id: student.studentId,
+      1: '',
+      2: '',
+      3: '',
+      4: '',
+      5: '',
+      6: '',
+      7: '',
+      8: '',
+      9: '',
+      10: '',
+      11: '',
+      12: '',
+      13: '',
+      14: '',
+      15: '',
+      16: '',
+      17: '',
+      18: '',
+      19: '',
+      20: '',
+      21: '',
+      22: '',
+      23: '',
+      24: '',
+      25: '',
+      26: '',
+      27: '',
+    }
+    for (score of scores) {
+      if (score.studentId === student.studentId) {
+        aggScore[score.questionId] = score.score
+      }
+    }
+    allScores.push(aggScore);
+  }
+
+  console.log(allScores);
+
+  // sends batch id or 'none'
+  try {
+    res.send(scores);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+
+  // .then((result) => {
+  // console.log('result', result.rows);
+
+  // })
+  // .catch((err) => {
+  // res.sendStatus(500);
+  // });
+
+}); // end Get student Csv export---
 
 // ------------------------------Post Route for student CSV import/upload---------------------------
 router.post("/", async (req, res) => {
   const connection = await pool.connect();
 
-// ==== NEED TO ADD IS AUTHENTICATED ====
+  // ==== NEED TO ADD IS AUTHENTICATED ====
 
   try {
     await connection.query('BEGIN');
     console.log("post route hit server side");
     console.log('req.body is', req.body);
-    
-    // CREATE NEW USER LOOP ================== 
-    for (let student of req.body.studentArray){
 
-    // Username is email
+    // CREATE NEW USER LOOP ================== 
+    for (let student of req.body.studentArray) {
+
+      // Username is email
       const username = student.email;
-      
+
       // Password encryption
       const password = encryptLib.encryptPassword(student.firstName + student.studentId);
-      
+
       console.log("username is", username);
       console.log("password is", password);
-      
+
       const sqlAddUser = `INSERT INTO "user" (username, password)
       VALUES ($1, $2) RETURNING id;`;
-      
+
       const userId = await connection.query(sqlAddUser, [username, password]);
-      
-      
+
+
       /// ======= INSERT INTO STUDENTS TABLE WITH RETURNED ID
-      
+
       const sqlAddStudent = `INSERT INTO "students" ("userId","studentId", "firstName", "lastName", "graduationYear", "email", "race", "eip", "gender", "lunchStatus", "schoolId") 
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);`;
-      
+
       let queryInserts = [
         userId.rows[0].id,
         student.studentId,
