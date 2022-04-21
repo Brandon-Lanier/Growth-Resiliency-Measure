5e5b0f88f108f9e36b8b2981c5846cd9c69f2eea
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-04-21 10:53:24 -0500

    cleaned up router code

--- server/routes/dataViz.router.js
+++ server/routes/dataViz.router.js
@@ -375,78 +375,10 @@ router.get("/export", async (req, res) => {
         "questions"."measureName";
         `;
 
-
-        console.log('qryText', qryTextOne + qryTextTwo + qryTextThree)
-        console.log('qryArguments is', qryArguments)
-
         // get data from database
         const query = await pool.query(qryTextOne + qryTextTwo + qryTextThree, qryArguments);
         const queryObjects = query.rows;
 
-        // organize data into arrays based off time frames
-        // first get a list of all academic years in the data
-        let years = queryObjects.map(function (object) {
-            return object.fiscalYear;
-        });
-        let uniqueYears = [...new Set(years)];
-
-        // create new arrays of data based on year, term, or batch
-        let allYears = [];
-        let terms = [1, 2];
-        let batches = [1, 2];
-        // switch (req.query.timeFrames) {
-        //     case 'year':
-        //         // create new arrays of data based on year
-        //         for (let year of uniqueYears) {
-        //             let array = []
-        //             for (let object of queryObjects) {
-        //                 if (object.fiscalYear == year) {
-        //                     array.push(object)
-        //                 }
-        //             }
-        //             // push new array of data into allYears
-        //             allYears.push(array)
-        //         }
-        //         break;
-        //     case 'term':
-        //         // create new arrays of data based on year and term
-        //         for (let year of uniqueYears) {
-        //             for (let term of terms) {
-        //                 let array = []
-        //                 for (let object of queryObjects) {
-        //                     if (object.fiscalYear == year && object.semesterNumber == term) {
-        //                         array.push(object)
-        //                     }
-        //                 }
-        //                 allYears.push(array)
-        //             }
-        //             // push new array of data into allYears
-        //         }
-        //         break;
-        //     case 'batch':
-        //         // create new arrays of data based on year, term, and batch
-        //         for (let year of uniqueYears) {
-        //             for (let term of terms) {
-        //                 for (let batch of batches) {
-        //                     let array = []
-        //                     for (let object of queryObjects) {
-        //                         if (object.fiscalYear == year && object.semesterNumber == term && object.batchNumber == batch) {
-        //                             array.push(object)
-        //                         }
-        //                     }
-        //                     allYears.push(array)
-        //                 }
-        //             }
-        //             // push new array of data into allYears
-        //         }
-        //         break;
-        // }
-
-        console.log('QUERY OBJECTS ARE', queryObjects)
-        // for (let object of queryObjects) {
-
-        // }
-        // sends query in correct form
         try {
             res.send(queryObjects);
         } catch (err) {
