45a2906c9234b5f161bb64db4498a5358794da93
Brandon Lanier <brandonjlanier@gmail.com>
2022-04-21 21:41:03 -0500

    Styled admin admin component

--- src/components/AdminControl/AdminTable.jsx
+++ src/components/AdminControl/AdminTable.jsx
@@ -1,16 +1,36 @@
 import * as React from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
-import axios from "axios";
+import { styled } from '@mui/material/styles';
 
 export default function BasicTable({ admins, school, removeAdmin }) {
   //  const filterAdmin = admins.filter()
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: "rgba(17, 24, 39, 0.7)",
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
 
   return (
     <>
@@ -18,10 +38,10 @@ export default function BasicTable({ admins, school, removeAdmin }) {
         <Table sx={{ minWidth: 300 }} size="small" aria-label="a dense table">
           <TableHead>
             <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell align="left">Email</TableCell>
-              <TableCell align="left">School</TableCell>
-              <TableCell align="right"></TableCell>
+              <StyledTableCell align="left">Name</StyledTableCell>
+              <StyledTableCell align="left">Email</StyledTableCell>
+              <StyledTableCell align="left">School</StyledTableCell>
+              <StyledTableCell align="right"></StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -32,14 +52,14 @@ export default function BasicTable({ admins, school, removeAdmin }) {
                   key={row.id}
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                 >
-                  <TableCell component="th" scope="row">
+                  <StyledTableCell component="th" scope="row">
                     {row.firstName} {row.lastName}
-                  </TableCell>
-                  <TableCell align="left">{row.email}</TableCell>
-                  <TableCell align="left">{row.name}</TableCell>
-                  <TableCell align="right">
-                    <Button onClick={() => removeAdmin(row.id)}>Remove</Button>
-                  </TableCell>
+                  </StyledTableCell>
+                  <StyledTableCell align="left">{row.email}</StyledTableCell>
+                  <StyledTableCell align="left">{row.name}</StyledTableCell>
+                  <StyledTableCell align="right">
+                  <Button onClick={() => removeAdmin(row.id)} variant="outlined">Remove</Button>
+                  </StyledTableCell>
                 </TableRow>
               ))}
           </TableBody>
